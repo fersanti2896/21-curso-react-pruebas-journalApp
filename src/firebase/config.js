ca03faaf7f89615bc4c3ca34e0dde7023dca5664
Fernando Nicolás <fersanti2896@gmail.com>
2023-03-09 17:45:18 -0600

    Pruebas en thunks.js de journal

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -2,14 +2,24 @@ import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore/lite'
 
-// Your web app's Firebase configuration
+// Pruducción
+// const firebaseConfig = {
+//   apiKey: "AIzaSyBHeJisgoW7IJXrAyMiM6_VzHimmPXEJoQ",
+//   authDomain: "react-curso-redux-1e2c0.firebaseapp.com",
+//   projectId: "react-curso-redux-1e2c0",
+//   storageBucket: "react-curso-redux-1e2c0.appspot.com",
+//   messagingSenderId: "1070029701562",
+//   appId: "1:1070029701562:web:c233ef2335a99bd1985d83"
+// };
+
+// Testing
 const firebaseConfig = {
-  apiKey: "AIzaSyBHeJisgoW7IJXrAyMiM6_VzHimmPXEJoQ",
-  authDomain: "react-curso-redux-1e2c0.firebaseapp.com",
-  projectId: "react-curso-redux-1e2c0",
-  storageBucket: "react-curso-redux-1e2c0.appspot.com",
-  messagingSenderId: "1070029701562",
-  appId: "1:1070029701562:web:c233ef2335a99bd1985d83"
+  apiKey: "AIzaSyBoLELo6Qqpt9xfiuzq5JMtuDuy5L8Gdyg",
+  authDomain: "react-journal-pruebas-1f688.firebaseapp.com",
+  projectId: "react-journal-pruebas-1f688",
+  storageBucket: "react-journal-pruebas-1f688.appspot.com",
+  messagingSenderId: "52575820962",
+  appId: "1:52575820962:web:fa1e53f8708c78b263cbfb"
 };
 
 export const FrebaseApp   = initializeApp( firebaseConfig );
--- src/tests/store/journal/thunks.test.js
+++ src/tests/store/journal/thunks.test.js
@@ -1,6 +1,42 @@
+import { collection, deleteDoc, getDocs } from 'firebase/firestore/lite';
+import { FirebaseDB } from '../../../firebase/config';
+import { startLoadingNotes } from '../../../store/auth';
+import { startNewNote, savingNewNote, addNewEmptyNote, setActiveNote, setNotes } from '../../../store/journal';
 
 describe('Pruebas en thunks de journal.', () => { 
-    test('Debe de.', () => { 
+    const dispatch = jest.fn();
+    const getState = jest.fn();
 
+    beforeEach( () => jest.clearAllMocks() );
+
+    test('startNewNote debe de crear una nueva nota.', async() => { 
+        const uid = 'TEST-UID';
+        getState.mockReturnValue({ auth: { uid: uid } });
+
+        await startNewNote()( dispatch, getState );
+
+        expect( dispatch ).toHaveBeenCalledWith( savingNewNote() );
+        expect( dispatch ).toHaveBeenCalledWith( addNewEmptyNote({
+            body: '',
+            title: '',
+            id: expect.any( String ),
+            date: expect.any( Number )
+        }) );
+        expect( dispatch ).toHaveBeenCalledWith( setActiveNote({
+            body: '',
+            title: '',
+            id: expect.any( String ),
+            date: expect.any( Number )
+        }) );
+
+        /* Borrar de firebase */
+        const collectionRef = collection( FirebaseDB, `${ uid }/journal/notes` );
+        const docs = await getDocs( collectionRef );
+
+        const deletePromises = [];
+        docs.forEach( doc => deletePromises.push( deleteDoc( doc.ref ) ) );
+
+        /* Borra toda la colección */
+        await Promise.all( deletePromises );
     });
 });
